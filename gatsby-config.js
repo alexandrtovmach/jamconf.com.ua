cc53ff11a6db61ee9888810cc0f6db2f4dd828f6
alexandrtovmach <alexandrtovmach@gmail.com>
2019-12-03 22:36:33 +0200

    feat: Completed start page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `JAMConf in Ukraine`,
+    description: `The first JAMstack conference in Ukraine`,
+    author: `@alexandrtovmach`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
